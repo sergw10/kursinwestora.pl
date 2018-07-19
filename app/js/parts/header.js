57aa41b88e138a369a253545da13d9666a073cc4
Sergey <sergw100@gmail.com>
2018-07-20 03:35:30 +0600

    add tablet and mobile respons

--- app/js/parts/header.js
+++ app/js/parts/header.js
@@ -1,14 +1,29 @@
 $(function() {
 
-	$('.header-submenu__item, .header-menu__item').hover(function() {
-		$(this).addClass('hover');
-	}, function() {
-		$(this).removeClass('hover');
-	});
-
 	// Burger menu open
   $('.burger-menu__wrap').click(function() {
-  	$(this).toggleClass('open');
+  	$(this).toggleClass('js_open');
+  	$('.header-menu').toggleClass('js_show');
   });
+  
+  if ( $('html').width() >= 976 ) {
+
+		// Hover event in main menu
+		$('.header-submenu__item, .header-menu__item').hover(function() {
+			$(this).addClass('js_hover');
+		}, function() {
+			$(this).removeClass('js_hover');
+		});
+  }
+
+  if ( $('html').width() <= 975 ) {
+
+  	// Click on arrow in main menu (mobile version)
+  	$('.header-menu__item-arrow').click(function() {
+  		$(this).toggleClass('js_rotate');
+  		$(this).parent().find('.header-submenu__wrap').toggleClass('js_show');
+  		$(this).parent().find('.header-menu__link').toggleClass('js_blue_background')
+  	});
+  }
 	
 });

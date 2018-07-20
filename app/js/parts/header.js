8790afb68128943cc905ab7ef022b82ea4b76d44
Sergey <sergw100@gmail.com>
2018-07-20 22:31:21 +0600

    hot fix

--- app/js/parts/header.js
+++ app/js/parts/header.js
@@ -6,24 +6,45 @@ $(function() {
   	$('.header-menu').toggleClass('js_show');
   });
   
-  if ( $('html').width() >= 976 ) {
-
+  var header_desctop_event = function () {
+	  $('.header-menu__item').removeClass('js_hover');
+	  $('.header-menu__item-arrow').click(function(e) {
+	  	e.preventDefault();
+	  });
 		// Hover event in main menu
-		$('.header-submenu__item, .header-menu__item').hover(function() {
-			$(this).addClass('js_hover');
-		}, function() {
-			$(this).removeClass('js_hover');
-		});
-  }
+	  if ( $('html').width() >= 976 ) {
+			$('.header-submenu__item, .header-menu__item').hover(function() {
+				$(this).addClass('js_hover');
+			}, function() {
+				$(this).removeClass('js_hover');
+			});
+	  }
+	}
+	header_desctop_event();
 
-  if ( $('html').width() <= 975 ) {
+  var header_mobile_event = function () {
+		$('.header-submenu__item, .header-menu__item').hover(function(e) {
+	  	e.preventDefault();
+	  });
+	  // Click on arrow in main menu (mobile version)
+	  if ( $('html').width() <= 975 ) {
+	  	$('.header-menu__item-arrow').click(function() {
+	  		$(this).toggleClass('js_rotate');
+	  		$(this).parent().find('.header-submenu__wrap').toggleClass('js_show');
+	  		$(this).parent().find('.header-menu__link').toggleClass('js_blue_background')
+	  	});
+	  }
+	}
+	header_mobile_event();
 
-  	// Click on arrow in main menu (mobile version)
-  	$('.header-menu__item-arrow').click(function() {
-  		$(this).toggleClass('js_rotate');
-  		$(this).parent().find('.header-submenu__wrap').toggleClass('js_show');
-  		$(this).parent().find('.header-menu__link').toggleClass('js_blue_background')
-  	});
-  }
+	// $(window).on("orientationchange",function(){
+	$(window).resize(function(){
+		if (window.matchMedia("(orientation: portrait)").matches) {
+			header_mobile_event();
+    }
+    if (window.matchMedia("(orientation: landscape)").matches) {
+			header_desctop_event();
+    }
+  });
 	
 });

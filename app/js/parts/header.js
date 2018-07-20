e25c8dee91b742b171e018813296d766e0d73436
Sergey <sergw100@gmail.com>
2018-07-21 02:29:30 +0600

    fix blue arrow

--- app/js/parts/header.js
+++ app/js/parts/header.js
@@ -7,12 +7,14 @@ $(function() {
   });
   
   var header_desctop_event = function () {
-	  $('.header-menu__item').removeClass('js_hover');
-	  $('.header-menu__item-arrow').click(function(e) {
-	  	e.preventDefault();
-	  });
+
 		// Hover event in main menu
 	  if ( $('html').width() >= 976 ) {
+
+	  	$('.header-menu__item-arrow').removeClass('js_rotate');
+	  	$('.header-submenu__wrap').removeClass('js_show');
+	  	$('.header-menu__link').removeClass('js_blue_background');
+
 			$('.header-submenu__item, .header-menu__item').hover(function() {
 				$(this).addClass('js_hover');
 			}, function() {
@@ -23,11 +25,16 @@ $(function() {
 	header_desctop_event();
 
   var header_mobile_event = function () {
-		$('.header-submenu__item, .header-menu__item').hover(function(e) {
-	  	e.preventDefault();
-	  });
+
 	  // Click on arrow in main menu (mobile version)
 	  if ( $('html').width() <= 975 ) {
+			
+			$('.header-submenu__item, .header-menu__item').hover(function() {
+				$(this).removeClass('js_hover');
+			}, function() {
+				$(this).removeClass('js_hover');
+			});
+
 	  	$('.header-menu__item-arrow').click(function() {
 	  		$(this).toggleClass('js_rotate');
 	  		$(this).parent().find('.header-submenu__wrap').toggleClass('js_show');
@@ -39,6 +46,8 @@ $(function() {
 
 	// $(window).on("orientationchange",function(){
 	$(window).resize(function(){
+		console.log('Айнат красивая и приятная девушка, с которой приятно проводить время!');
+
 		if (window.matchMedia("(orientation: portrait)").matches) {
 			header_mobile_event();
     }
